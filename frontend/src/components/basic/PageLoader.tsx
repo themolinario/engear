70a518bc297ab93db943ebf356833a229772c44c
Sergio <sergmose@gmail.com>
2023-10-14 19:29:18 +0200

    feta: added style to CircularProgress

--- frontend/src/components/basic/PageLoader.tsx
+++ frontend/src/components/basic/PageLoader.tsx
@@ -1,7 +1,17 @@
-import {CircularProgress} from "@mui/material";
+import { CircularProgress } from "@mui/material";
 
-export function PageLoader () {
-    return (
-        <CircularProgress />
-    )
+const indicatorSize = 40;
+
+export function PageLoader() {
+  return (
+    <CircularProgress
+      size={indicatorSize}
+      sx={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      marginTop: `${-indicatorSize / 2}px`,
+      marginLeft: `${-indicatorSize / 2}px`
+    }} />
+  );
 }
