7d9b53438b372bf4d4a8f7979cb3fe5b24d18d1a
Sergio <sergmose@gmail.com>
2023-10-15 09:25:23 +0200

    feta: added style to Upload video

--- frontend/src/pages/AddVideoPage/index.tsx
+++ frontend/src/pages/AddVideoPage/index.tsx
@@ -1,178 +1,220 @@
-
 import Button from "@mui/material/Button";
-import {FormEvent, useEffect, useState} from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
-import FileUploadIcon from '@mui/icons-material/FileUpload';
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 import Typography from "@mui/material/Typography";
 import {
-    getStorage,
-    ref,
-    uploadBytesResumable,
-    getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL
 } from "firebase/storage";
 import TextField from "@mui/material/TextField";
-import {useMutation} from "@tanstack/react-query";
-import {postVideo} from "../../api/videos.ts";
-import {IVideo} from "../../types/Video.ts";
-import {app} from "../../firebaseConfig.ts";
-import {useNavigate} from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { postVideo } from "../../api/videos.ts";
+import { IVideo } from "../../types/Video.ts";
+import { app } from "../../firebaseConfig.ts";
+import { useNavigate } from "react-router-dom";
+import { LinearProgress } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 
+export function AddVideoPage() {
+  const storage = getStorage(app);
+  const [video, setVideo] = useState<File | undefined>();
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [img, setImg] = useState<File | undefined>();
+  const [imgPerc, setImgPerc] = useState(0);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [videoURL, setVideoURL] = useState("");
+  const [imgURL, setImgURL] = useState("");
+  const navigate = useNavigate();
+
+  const uploadVideoMutation = useMutation({
+    mutationFn: (video: Partial<IVideo>) => postVideo(video),
+    onSuccess: (res) => {
+      navigate(`/${res.data._id}`);
+    }
+  });
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    uploadVideoMutation.mutate({
+      title: title,
+      desc: desc,
+      videoUrl: videoURL,
+      imgUrl: imgURL
+    });
+
+  };
+
+  useEffect(() => {
+    if (video) {
+      const videoRef = ref(storage, video.name + new Date().getTime());
+      const videoUpload = uploadBytesResumable(videoRef, video);
+      videoUpload.on("state_changed", (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setVideoPerc(progress);
+        },
+        () => {
+        },
+        () => {
+          getDownloadURL(videoUpload.snapshot.ref).then((downloadURL) => setVideoURL(downloadURL));
+        });
+
+    }
 
 
-export function AddVideoPage () {
-    const storage = getStorage(app);
-    const [video, setVideo] = useState<File | undefined>();
-    const [videoPerc, setVideoPerc] = useState(0);
-    const [img, setImg] = useState<File | undefined>();
-    const [imgPerc, setImgPerc] = useState(0);
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const [videoURL, setVideoURL] = useState("");
-    const [imgURL, setImgURL] = useState("");
-    const navigate = useNavigate();
+  }, [video]);
 
-    const uploadVideoMutation = useMutation({
-        mutationFn: (video: Partial<IVideo>) => postVideo(video),
-        onSuccess: (res) => {
-            navigate(`/${res.data._id}`)
-        }
-    })
+  useEffect(() => {
+    if (img) {
+      const imgRef = ref(storage, img.name + new Date().getTime());
+      const imgUpload = uploadBytesResumable(imgRef, img);
+      imgUpload.on("state_changed", (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImgPerc(progress);
+      }, () => {
+      }, () => {
+        getDownloadURL(imgUpload.snapshot.ref).then((downloadURL) => setImgURL(downloadURL));
+      });
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-            uploadVideoMutation.mutate({
-                title: title,
-                desc: desc,
-                videoUrl: videoURL,
-                imgUrl: imgURL
-            })
-
-    };
-
-    useEffect(() => {
-        if (video) {
-            const videoRef = ref(storage,video.name + new Date().getTime());
-            const videoUpload = uploadBytesResumable(videoRef, video);
-            videoUpload.on("state_changed", (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
-                setVideoPerc(progress);
-            },
-                () => {},
-                () => {getDownloadURL(videoUpload.snapshot.ref).then((downloadURL) => setVideoURL(downloadURL))});
-
-        }
+    }
 
 
-    }, [video]);
-
-    useEffect(() => {
-        if (img) {
-            const imgRef = ref(storage, img.name + new Date().getTime());
-            const imgUpload = uploadBytesResumable(imgRef, img);
-            imgUpload.on("state_changed", (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes ) * 100;
-                setImgPerc(progress);
-            }, () => {}, () => {
-                getDownloadURL(imgUpload.snapshot.ref).then((downloadURL) => setImgURL(downloadURL));
-            });
-
-        }
+  }, [img]);
 
 
-    }, [img]);
-
-
-
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <FileUploadIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Upload A Video
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Typography>Pick Video File: </Typography>
+          <Box
+            sx={{
+              marginTop: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              borderColor: "blue",
+              padding: 4,
+              border: 1,
+              borderRadius: 2,
+              marginBottom: 2
+            }}
+          >
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+              Upload Video {videoPerc}
+              <VisuallyHiddenInput
+                type="file"
+                accept="video/*"
+                id="video"
+                name="video"
+                required
+                hidden
+                onChange={(e) => {
+                  if (e.target.files) setVideo(e.target.files[0]);
                 }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <FileUploadIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Upload A Video
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <Typography >Pick Video File: </Typography>
-                    <Box
-                        sx={{
-                            marginTop: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            borderColor: "blue",
-                            padding: 2,
-                            border: 4,
-                            borderRadius: 2,
-                            marginBottom: 2
-                        }}
-                    >
-                        <input
-                            type="file"
-                            accept="video/*"
-                            id="video"
-                            name="video"
-                            required
-                            onChange={(e) => {if (e.target.files) setVideo(e.target.files[0])}}
-                        />
-                        {videoPerc}
-                    </Box>
-                    <Typography >Set Preview Image: </Typography>
-                    <Box
-                        sx={{
-                            marginTop: 2,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            borderColor: "blue",
-                            padding: 2,
-                            border: 4,
-                            borderRadius: 2
-                        }}
-                    >
+              />
+              <LinearProgress variant="determinate" value={videoPerc} />
+            </Button>
 
-                        <input
-                            type="file"
-                            required
-                            accept="image/*"
-                            id="image"
-                            name="image"
-                            onChange={(e) => {if (e.target.files) setImg(e.target.files[0])}}
-                        />
-                        {imgPerc}
-                    </Box>
-                    <TextField
-                        id="title"
-                        placeholder="Inserisci titolo"
-                        onChange={e => setTitle(e.target.value)}
-                    />
-                    <TextField
-                        id="description"
-                        placeholder="Inserisci descrizione"
-                        onChange={e => setDesc(e.target.value)}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Publish
-                    </Button>
-                </Box>
-            </Box>
-        </Container>
-    );
+            {video?.name}
+
+          </Box>
+          <Typography>Set Preview Image: </Typography>
+          <Box
+            sx={{
+              marginTop: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              borderColor: "blue",
+              padding: 4,
+              border: 1,
+              borderRadius: 2,
+              marginBottom: 2
+            }}
+          >
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+              Upload Image {imgPerc}
+
+              <VisuallyHiddenInput
+                type="file"
+                required
+                accept="image/*"
+                id="image"
+                name="image"
+                onChange={(e) => {
+                  // console.log(e.target.files[0])
+                  if (e.target.files) setImg(e.target.files[0]);
+                }}
+              />
+              {/*<LinearProgress variant="determinate" value={imgPerc} />*/}
+
+            </Button>
+
+            {img?.name}
+
+          </Box>
+          <TextField
+            id="title"
+            placeholder="Inserisci titolo"
+            fullWidth
+            margin="normal"
+            onChange={e => setTitle(e.target.value)}
+          />
+          <TextField
+            id="description"
+            placeholder="Inserisci descrizione"
+            multiline
+            maxRows={4}
+            fullWidth
+            margin="normal"
+            onChange={e => setDesc(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            size="large"
+          >
+            Publish
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1
+});
