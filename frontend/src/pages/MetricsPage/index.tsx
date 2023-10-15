6367489c4227331735da401068d17006256fc0a0
MarcoMolinari <themolinario@gmail.com>
2023-10-15 12:57:12 +0200

    Feature: added chart for download rate

--- frontend/src/pages/MetricsPage/index.tsx
+++ frontend/src/pages/MetricsPage/index.tsx
@@ -5,6 +5,11 @@ import { getCurrentUser, getSpeedTest } from "../../api/user.ts";
 import { formatTime, millisToMinutesAndSeconds } from "../../utils/utils.ts";
 import { useEffect, useState } from "react";
 import BasicTable from "./components/BasicTable.tsx";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 const INIT_VALUE = {
   ip: "N.N.",
@@ -30,6 +35,8 @@ export function MetricsPage() {
         queryClient.fetchQuery(["user"], getCurrentUser),
         queryClient.fetchQuery(["speedTest"], getSpeedTest)
       ]);
+
+
 
       setMetrics({
         ip: ipResult.data.ip ?? "N.D.",
@@ -89,11 +96,45 @@ export function MetricsPage() {
     "Speed test"
   ];
 
+  const metricsData = {
+    labels: ["Download Rate"],
+    datasets: [
+      {
+        label: "Download rate",
+        data: [metrics.speedTest && Number(metrics.speedTest.substring(0, metrics.speedTest.length - 3))],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      }
+    ],
+
+  }
 
   if (loading) return <PageLoader />;
 
   return (
-    <BasicTable header={header} rows={rows}></BasicTable>
+    <>
+      <BasicTable header={header} rows={rows}></BasicTable>
+      <div style={{width: 500}}>
+        <Doughnut data={metricsData} />
+      </div>
+
+    </>
+
     // <div>
     //   <h1>Your IP Address is: {metrics.ip}</h1>
     //   <h1>Your User Agent is: {metrics.userAgent}</h1>
